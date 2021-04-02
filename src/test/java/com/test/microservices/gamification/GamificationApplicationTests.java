b0b12e0a9779ba320f49f1916ad6906802a937c5
Github Hind <githubhind@gmail.com>
2021-04-02 23:42:52 +0200

    Initial commit

---
+++ src/test/java/com/test/microservices/gamification/GamificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.microservices.gamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
